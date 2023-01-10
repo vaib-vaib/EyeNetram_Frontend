c17ebc109b3bc2d103e50929f21db7fec5b54b07
unknown <ambarkarvaibhavi@gmail.com>
2023-01-10 13:01:56 +0530

    Fourth Commit

--- src/components/upload-form.jsx
+++ src/components/upload-form.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-  
+import { SubmitButton } from "./submitButton";
+
 export function UploadImages(){
+
     const styles = {
         button: {
             padding:"5px 10px",
@@ -15,22 +17,25 @@ export function UploadImages(){
         },
       }
     const [file, setFile] = useState();
-    const [success, setSuccess] = React.useState(false);
-
+    
     function handleChange(e) {
         console.log(e.target.files);
         setFile(URL.createObjectURL(e.target.files[0]));
     }
   
-    function myFunction(){
-        const newLocal = "Image submitted sucessfully";
-        alert(newLocal);
-    }
+    async function handleSubmit(event) {
+        event.preventDefault()
+        const result = await onSubmit(UploadImage)
+        console.log(result)
+        setSuccess(true)
+      } 
+    
     return (
-        <div className="m-0 mt-8 max-w-md md:flex" style={styles.button}>
-            <input type="file" name = "UploadedImage" accept="image/x-png,image/jpg,image/jpeg" onChange={handleChange} onclick="myFunction()" />
-            <img src={file}/>
-        </div>
-  
+        <><div className="m-0 mt-8 max-w-md md:flex" style={styles.button}>
+
+            <input type="file" name="UploadedImage" accept="image/x-png,image/jpg,image/jpeg"/>
+            <img src={file} />
+        </div><SubmitButton /></>
+        
     );
 }
